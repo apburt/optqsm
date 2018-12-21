4bbf333dc656f13ad72bf9e3c482146de694d136
apburt <a.burt@ucl.ac.uk>
2018-12-21 18:57:24 +0000

    select patchDiam via dNNz percentiles

--- src/optInputs.m
+++ src/optInputs.m
@@ -3,15 +3,15 @@
 function [inputs] = optInputs(cname,dNNz1,dNNz2)
 	inputs = struct([]);
 	%variation in considered pd1 patch sizes: 150-300% dNNz1
-	pd1 = round(max(dNNz1),3); 
+	pd1 = round(prctile(dNNz1,75),3); 
 	pd1Steps = 4;
 	pd1Start = pd1 * 1.5;
 	pd1End = pd1 * 3.0;
 	pd1Int =  (pd1End - pd1Start) / (pd1Steps-1);
 	%
 	%variation in considered pd2 patch sizes: 150-300% dNNz2
-	pd2Min = round(min(dNNz2),3);
-	pd2Max = round(max(dNNz2),3);
+	pd2Min = round(prctile(dNNz2,25),3);
+	pd2Max = round(prctile(dNNz2,75),3);
 	pd2Steps = 6;
 	%
 	pd2MinStart = pd2Min * 1.5;
